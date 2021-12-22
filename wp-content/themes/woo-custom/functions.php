df89b6444e70879871ebe05b28070bdc7778a4f6
kingduy119 <hoangduy1192@gmail.com>
2021-12-22 14:21:13 +0700

    add theme woocommer custom

---
+++ wp-content/themes/woo-custom/functions.php
@@ -0,0 +1,102 @@
+<?php
+
+function custom_wc_theme_suppor()
+{
+    add_theme_support('woocomerce');
+    add_theme_support('wc-product-gallery-lightbox');
+    add_theme_support('wc-product-gallery-slider');
+}
+
+function init_theme()
+{
+    // Chuyển trình soạn thảo về phiên bản cũ
+    add_filter('use_block_editor_for_post', '__return_false');
+
+    // Menus:
+    register_nav_menu('header-top', __('Top menu'));
+    register_nav_menu('header-main', __('Main menu'));
+    register_nav_menu('footer-menu', __('Footer menu'));
+
+    // Sidebar:
+    if (function_exists('register_sidebar')) {
+        register_sidebar(array(
+            'name' => 'Sidebar',
+            'id' => 'sidebar',
+        ));
+    }
+
+    function set_postview($postID)
+    {
+        $count_key = 'views';
+        $count = get_post_meta($postID, $count_key, true);
+        if ($count == '') {
+            $count = 0;
+            delete_post_meta($postID, $count_key);
+            add_post_meta($postID, $count_key, '0');
+        } else {
+            $count++;
+            update_post_meta($postID, $count_key, $count);
+        }
+    }
+
+    function get_postview($postID)
+    {
+        $count_key = 'views';
+        $count = get_post_meta($postID, $count_key, true);
+        if ($count == '') {
+            delete_post_meta($postID, $count_key);
+            add_post_meta($postID, $count_key, '0');
+            return '0';
+        }
+        return $count;
+    }
+}
+add_action('init', 'init_theme');
+
+
+function tao_custom_post_type()
+{
+    /*
+     * Biến $label để chứa các text liên quan đến tên hiển thị của Post Type trong Admin
+     */
+    $label = array(
+        'name' => 'Các sản phẩm', //Tên post type dạng số nhiều
+        'singular_name' => 'Sản phẩm' //Tên post type dạng số ít
+    );
+ 
+    /*
+     * Biến $args là những tham số quan trọng trong Post Type
+     */
+    $args = array(
+        'labels' => $label, //Gọi các label trong biến $label ở trên
+        'description' => 'Post type đăng sản phẩm', //Mô tả của post type
+        'supports' => array(
+            'title',
+            'editor',
+            'excerpt',
+            'author',
+            'thumbnail',
+            'comments',
+            'trackbacks',
+            'revisions',
+            'custom-fields'
+        ), //Các tính năng được hỗ trợ trong post type
+        'taxonomies' => array( 'category', 'post_tag' ), //Các taxonomy được phép sử dụng để phân loại nội dung
+        'hierarchical' => false, //Cho phép phân cấp, nếu là false thì post type này giống như Post, true thì giống như Page
+        'public' => true, //Kích hoạt post type
+        'show_ui' => true, //Hiển thị khung quản trị như Post/Page
+        'show_in_menu' => true, //Hiển thị trên Admin Menu (tay trái)
+        'show_in_nav_menus' => true, //Hiển thị trong Appearance -> Menus
+        'show_in_admin_bar' => true, //Hiển thị trên thanh Admin bar màu đen.
+        'menu_position' => 5, //Thứ tự vị trí hiển thị trong menu (tay trái)
+        'menu_icon' => '', //Đường dẫn tới icon sẽ hiển thị
+        'can_export' => true, //Có thể export nội dung bằng Tools -> Export
+        'has_archive' => true, //Cho phép lưu trữ (month, date, year)
+        'exclude_from_search' => false, //Loại bỏ khỏi kết quả tìm kiếm
+        'publicly_queryable' => true, //Hiển thị các tham số trong query, phải đặt true
+        'capability_type' => 'post' //
+    );
+ 
+    register_post_type('slider', $args); //Tạo post type với slug tên là sanpham và các tham số trong biến $args ở trên
+}
+add_action('init', 'tao_custom_post_type');
---
+++ wp-content/themes/woo-custom/sidebar.php
@@ -0,0 +1,71 @@
+<div class="category-box">
+    <h3>Danh mục sản phẩm</h3>
+    <div class="content-cat">
+        <ul>
+            <li><i class="fa fa-angle-right"></i> <a href="#">Điện thoại</a></li>
+            <li><i class="fa fa-angle-right"></i> <a href="#">Máy tính bảng</a></li>
+            <li><i class="fa fa-angle-right"></i> <a href="#">laptop</a></li>
+            <li><i class="fa fa-angle-right"></i> <a href="#">Apple</a></li>
+            <li><i class="fa fa-angle-right"></i> <a href="#">Đồng hồ thông minh</a></li>
+            <li><i class="fa fa-angle-right"></i> <a href="#">Thiết bị đeo tay</a></li>
+            <li><i class="fa fa-angle-right"></i> <a href="#">Camera</a></li>
+            <li><i class="fa fa-angle-right"></i> <a href="#">Tivi</a></li>
+            <li><i class="fa fa-angle-right"></i> <a href="#">Sản phẩm khác</a></li>
+        </ul>
+    </div>
+</div>
+<div class="widget">
+    <h3>
+        <i class="fa fa-bars"></i>
+        Tin tức
+    </h3>
+    <div class="content-w">
+        <ul>
+            <li>
+                <a href="#"><img src="<?php bloginfo("stylesheet_directory") ?>/images/news.jpg" alt=""></a>
+                <h4><a href="#">Thương hiệu đồng hồ thông minh Sinophy của nước nào?</a></h4>
+                <div class="clear"></div>
+            </li>
+            <li>
+                <a href="#"><img src="<?php bloginfo("stylesheet_directory") ?>/images/news.jpg" alt=""></a>
+                <h4><a href="#">Thương hiệu đồng hồ thông minh Sinophy của nước nào?</a></h4>
+                <div class="clear"></div>
+            </li>
+            <li>
+                <a href="#"><img src="<?php bloginfo("stylesheet_directory") ?>/images/news.jpg" alt=""></a>
+                <h4><a href="#">Thương hiệu đồng hồ thông minh Sinophy của nước nào?</a></h4>
+                <div class="clear"></div>
+            </li>
+            <li>
+                <a href="#"><img src="<?php bloginfo("stylesheet_directory") ?>/images/news.jpg" alt=""></a>
+                <h4><a href="#">Thương hiệu đồng hồ thông minh Sinophy của nước nào?</a></h4>
+                <div class="clear"></div>
+            </li>
+            <li>
+                <a href="#"><img src="<?php bloginfo("stylesheet_directory") ?>/images/news.jpg" alt=""></a>
+                <h4><a href="#">Thương hiệu đồng hồ thông minh Sinophy của nước nào?</a></h4>
+                <div class="clear"></div>
+            </li>
+        </ul>
+    </div>
+</div>
+<div class="widget">
+    <h3>
+        <i class="fa fa-bars"></i>
+        Quảng cáo
+    </h3>
+    <div class="content-banner">
+        <a href="#">
+            <img src="<?php bloginfo("stylesheet_directory") ?>/images/banner.png" alt="">
+        </a>
+    </div>
+</div>
+<div class="widget">
+    <h3>
+        <i class="fa fa-facebook"></i>
+        Facebook
+    </h3>
+    <div class="content-fb">
+        <div class="fb-page" data-href="https://www.facebook.com/huykiradotnet/" data-tabs="timeline" data-width="" data-height="200" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"></div>
+    </div>
+</div>
